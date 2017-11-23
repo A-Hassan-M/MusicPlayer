fec898e717f8e165c1f155e26e548c248d9db24f
Amr Saeed <amrshosny@gmail.com>
2017-11-23 20:20:10 +0200

    Update Artist.py

--- Model/Artist.py
+++ Model/Artist.py
@@ -1,12 +1,7 @@
 class Artist:
     name =''
     date_of_birth = ''
-    songs = []
-
-    def __init__(self, name, date_of_birth, songs):
+    
+    def __init__(self, name, date_of_birth):
         self.name = name
         self.date_of_birth = date_of_birth
-        self.songs = songs
-
-    def addSong(self, song):
-        self.songs.append(song)
