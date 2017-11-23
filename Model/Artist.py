74d959cb5e63d399718942ee67a47f2945c874ad
Amr Saeed <amrshosny@gmail.com>
2017-11-23 20:26:37 +0200

    Update Artist.py

--- Model/Artist.py
+++ Model/Artist.py
@@ -1,7 +1,9 @@
 class Artist:
     name =''
     date_of_birth = ''
+    band = None;
     
-    def __init__(self, name, date_of_birth):
+    def __init__(self, name, date_of_birth, band):
         self.name = name
         self.date_of_birth = date_of_birth
+        self.band = band
