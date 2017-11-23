79376f6f1b7385da943875c4650bbd452a0f4ae4
Amr Saeed <amrshosny@gmail.com>
2017-11-23 19:49:44 +0200

    Update Song.py

--- Model/Song.py
+++ Model/Song.py
@@ -1,8 +1,7 @@
 class Song:
     name = ''
-    artists = []
     bands = []
-    album_title = ''
+    album = ''
     release_date = ''
     lyrics = ''
     genres = ''
@@ -10,7 +9,6 @@ class Song:
 
     def __init__(self, name, artists, bands, album_title, release_date, lyrics, genres, length):
         self.name = name
-        self.artists = artists
         self.bands = bands
         self.album_title = album_title
         self.release_date = release_date
